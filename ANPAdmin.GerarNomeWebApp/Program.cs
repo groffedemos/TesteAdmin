1bd51a296af4929a6d56a7ca0daff3654d9b3757
Renato Groffe <renato.groffe@yahoo.com.br>
2021-08-20 19:18:02 -0300

    Criação Aplicação Geração Nomes Web App

---
+++ ANPAdmin.GerarNomeWebApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ANPAdmin.GerarNomeWebApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("******** IMERSÃO AZURE DEVOPS ********");
+            Console.WriteLine("Nome sugerido para uma nova aplicação:");
+            Console.WriteLine();
+            Console.WriteLine($"imersao-azdo-{Guid.NewGuid().ToString().Substring(0, 8)}");
+            Console.WriteLine();
+        }
+    }
+}
